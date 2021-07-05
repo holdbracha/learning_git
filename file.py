bb0781b6b6b7b33431f2c8c61267cbd17cac690c
goldie l <g4199912@gmail.com>
2021-07-05 08:52:49 +0300

    add printsub function - merge

--- file.py
+++ file.py
@@ -5,3 +5,11 @@ print("new day")
 def print_range(num):
     for i in range(num):
         print(f'num: {i}')
+
+def printSubsequence(input, output):
+    if len(input) == 0:
+        print(output, end=' ')
+        return
+    printSubsequence(input[1:], output + input[0])
+    printSubsequence(input[1:], output)
+printSubsequence("goldie", "")
